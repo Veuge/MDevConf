4f24179c3f85a298c8e567f2f3d89923c1278d61
Veronica Clavijo <veuge.clavijo@gmail.com>
2022-12-02 15:26:44 -0400

    2.export-method

--- android/app/src/main/java/com/mdevconf/MyNativeModule.java
+++ android/app/src/main/java/com/mdevconf/MyNativeModule.java
@@ -1,9 +1,12 @@
 package com.mdevconf;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
 
 public class MyNativeModule extends ReactContextBaseJavaModule {
     MyNativeModule(ReactApplicationContext context) {
@@ -14,5 +17,10 @@ public class MyNativeModule extends ReactContextBaseJavaModule {
     @Override
     public String getName() {
         return "MyNativeModule";
+    }
+
+    @ReactMethod
+    public void simpleLogger() {
+        Log.d("MyNativeModule", "Log desde la el modulo nativo!");
     }
 }
